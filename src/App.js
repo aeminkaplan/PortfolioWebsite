ba73cd02acda4ff1409438518f3e8b4f4f6dd8c0
Ferhat Sencer <narifjr@gmail.com>
2022-05-15 20:28:27 +0300

    Login and Form submit componenets added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,76 @@ import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 
-//import React, { useState } from "react";
+import ContactForm from './components/ContactForm';
+
 import ReactDOM from "react-dom";
 
 import "./styles-login.css";
 class App extends Component {
+
+  // const [errorMessages, setErrorMessages] = useState({});
+  // const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // database = [
+  //   {
+  //     username: "user1",
+  //     password: "pass1"
+  //   },
+  //   {
+  //     username: "user2",
+  //     password: "pass2"
+  //   }
+  // ];
+
+  // errors = {
+  //   uname: "invalid username",
+  //   pass: "invalid password"
+  // };
+
+  // handleSubmit = (event) => {
+
+  //   event.preventDefault();
+
+  //   var { uname, pass } = document.forms[0];
+
+  //   const userData = database.find((user) => user.username === uname.value);
+
+  //   if (userData) {
+  //     if (userData.password !== pass.value) {
+  //       setErrorMessages({ name: "pass", message: errors.pass });
+  //     } else {
+  //       setIsSubmitted(true);
+  //     }
+  //   } else {
+  //     setErrorMessages({ name: "uname", message: errors.uname });
+  //   }
+  // };
+
+  // renderErrorMessage = (name) =>
+  //   name === errorMessages.name && (
+  //     <div className="error">{errorMessages.message}</div>
+  //   );
+
+  // renderForm = (
+  //   <div className="form">
+  //     <form onSubmit={handleSubmit}>
+  //       <div className="input-container">
+  //         <label>Username </label>
+  //         <input type="text" name="uname" required />
+  //         {renderErrorMessage("uname")}
+  //       </div>
+  //       <div className="input-container">
+  //         <label>Password </label>
+  //         <input type="password" name="pass" required />
+  //         {renderErrorMessage("pass")}
+  //       </div>
+  //       <div className="button-container">
+  //         <input type="submit" />
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
+
   render() {
     return (
       <div className="App">
@@ -18,95 +83,15 @@ class App extends Component {
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>                 
+        {/* <ContactForm resumeData={resumeData}/>            */}
+        {/* <div className="login-form">
+          <div className="title">Sign In</div>
+          {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+        </div> */}
       </div>
     );
   }
 }
-
-function AppLogin() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
-
-  return (
-    <div className="app">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
 
 export default App;
