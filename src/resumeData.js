96c358be4947efb9b8dde9f03346b69fc1ad7037
Ferhat Sencer <narifjr@gmail.com>
2022-05-15 17:51:11 +0300

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,81 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Ferhat Sencer",
+    "role": "Software Engineer, you can contact me at below links.",
+    "linkedinId":" ",
+    "skypeid": " ",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ferhatsencer/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ferhatsencer",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I'm solution-oriented, self-learner, and a total team player Software Developer. I always try new ideas and am open to new technologies.",
+    "address":"Istanbul, Turkey",
+    "website":"+90552XXXXXXX, xxxxxx@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Sakarya University",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2022",
+        "Achievements":"Developed various projects/homeworks in C, C++, Java, Python, and JavaScript."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Eyedius Technology",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"Developed an object detection application with C++/Qt using yolov5 models."
+      },
+      {
+        "CompanyName":"Eretna Medical Devices",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2021",
+        "Achievements":"Worked with Qt/C++ applications, bug fixing, feature adding, etc.\n" +
+                        "Developed cross-platform remote monitoring C++/QT application.\n" +
+                        "Built custom Linux OS for Radxa Rock and Raspberry Pi boards using Buildroot Tool.\n" +
+                        "Wrote Bash scripts for automating custom Linux building processes."
+      }
+    ],
+    "skillsDescription":"Skills",
+    "skills":[
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Qt/QML"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Bash, Linux, Buildroot"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"rostool",
+        "description":"bash script",
+        "imgurl":"images/bashscript.jpg"
+      },
+      {
+        "name":"web crawler",
+        "description":"CLI app",
+        "imgurl":"images/web-crawler.jpg",
+        "url":"https://github.com/ferhatsencer/web_crawler"
+      }
+    ]
+  }
+  
+  export default resumeData
